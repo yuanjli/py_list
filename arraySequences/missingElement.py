8cbdfef3a2c076f5d439eab3fdc522099040dbdc
yuanjli <liyuanjienetwork@gmail.com>
2018-11-24 23:21:44 -0800

    added linear to missing element

--- arraySequences/missingElement.py
+++ arraySequences/missingElement.py
@@ -19,3 +19,23 @@ def finder(arr1,arr2):
     return arr1[-1]
 
 print(finder([1,2,3,4,5,6,7],[3,7,2,1,4,6]))
+
+
+
+import collections
+
+def finder2(arr1, arr2):
+
+    d = collections.defaultdict(int)
+
+    for num in arr2:
+        d[num] += 1
+    for num in arr1:
+        if d[num] == 0:
+            return num
+        else:
+            d[num]-=1
+
+a1=[5,6,7,7]
+a2=[5,7,7]
+print(finder2(a1, a2))
